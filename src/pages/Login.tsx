37ccb701c17f7aed0e20c9e709e92624e984a099
Pratik <pratiikpatel.001@gmail.com>
2025-12-28 00:05:54 +0530

    Add login page layout

---
+++ src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import logo from "../assets/logo.png";
+const Login = () => {
+  return (
+    <>
+      <div>
+        <img src={logo} alt="Brand Logo" />
+        <div>
+          <h3 className="">Log In</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
