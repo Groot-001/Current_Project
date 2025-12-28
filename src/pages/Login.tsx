afd24482294b35f45253614fb73c8da7f1973e28
Pratik <pratiikpatel.001@gmail.com>
2025-12-28 16:51:36 +0530

    Update Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,10 +2,22 @@ import logo from "../assets/logo.png";
 const Login = () => {
   return (
     <>
-      <div>
-        <img src={logo} alt="Brand Logo" />
-        <div>
+      <div className="w-full">
+        <div className="w-32 ">
+          <div className="justify-center items-center">
+            <img src={logo} alt="Brand Logo" className="w-64" />
+          </div>
           <h3 className="">Log In</h3>
+          <form action="login form">
+            <div>
+              <label htmlFor="Email">Email / User Id</label>
+              <input type="email" placeholder="Enter Your Email or User ID" />
+            </div>
+            <div>
+              <label htmlFor="Password">Password</label>
+              <input type="password" placeholder="**********" />
+            </div>
+          </form>
         </div>
       </div>
     </>
