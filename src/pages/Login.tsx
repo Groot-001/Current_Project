283c6d13176459f91cb26c613a92d41f97719737
Pratik <pratiikpatel.001@gmail.com>
2026-01-04 00:50:35 +0530

    Logos added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import logo from "../assets/logo.png";
+import eye from "../assets/eye.svg";
 const Login = () => {
   return (
     <>
@@ -13,19 +14,26 @@ const Login = () => {
               <div className="flex flex-col">
                 <label htmlFor="Email">Email / User ID</label>
                 <input
+                  className="border-2 rounded-md p-1"
                   type="email"
                   id="Email"
                   placeholder="Enter Your Email or User ID"
                   required
                 />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col relative">
                 <label htmlFor="Password">Password</label>
                 <input
+                  className="border-2 rounded-md p-1"
                   type="password"
                   id="Password"
-                  placeholder="************************************"
+                  placeholder="*********"
                   required
+                />
+                <img
+                  src={eye}
+                  alt="eye logo"
+                  className="w-4 absolute right-4 top-8"
                 />
               </div>
               <div className="flex justify-between">
@@ -33,11 +41,13 @@ const Login = () => {
                   <div className="w-4 border-gray-400 h-4 bg-gray-500"></div>
                   <span>Remember me</span>
                 </div>
-                <div>
-                  <span>Forget me</span>
+                <div className="">
+                  <span className="text-[#A03879]">Forget Password?</span>
                 </div>
               </div>
-              <button className="text-white bg-blue-500 w-52">Login</button>
+              <button className="text-white bg-[#A03879] w-full p-1 rounded-md">
+                Login
+              </button>
             </form>
           </div>
         </div>
