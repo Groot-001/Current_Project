1377303db89987aa11b7bd9b0fed410d8578f1e9
Pratik <pratiikpatel.001@gmail.com>
2026-01-08 01:43:26 +0530

    Dashboard header layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import logo from "../assets/logo.png";
+import hamburger from "../assets/hamburger.svg";
+import minimize from "../assets/minimize.svg";
+import bell from "../assets/bell.svg";
+import notification from "../assets/99+.svg";
+import profileimage from "../assets/short image.svg";
+import down from "../assets/chevron-down.svg";
 
 const Dashboard = () => {
-  return <>Hello there!</>;
+  return (
+    <>
+      {/* Main Container */}
+      <div className="bg-slate-100 min-h-screen">
+        {/* Header */}
+        <div className="flex justify-between px-10 py-4">
+          <div className="flex justify-center">
+            <img src={logo} alt="Logo Image" className="w-44 mx-3" />
+            <img src={hamburger} alt="hamburger icon" className="w-6" />
+          </div>
+          <div className="flex justify-center gap-6">
+            <img src={minimize} alt="minimize icon" className="w-5" />
+            <img src={bell} alt="bell" className="w-6" />
+            <img src={notification} alt="Notification 99+" className="w-2" />
+            <img src={profileimage} alt="profile icon" className="w-4" />
+            <img src={down} alt="Down Arrow" className="w-4" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
