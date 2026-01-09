2841a876fbd354bb5221f45a12b4a4c735720019
Pratik <pratiikpatel.001@gmail.com>
2026-01-09 21:59:37 +0530

    updating header

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,7 +2,6 @@ import logo from "../assets/logo.png";
 import hamburger from "../assets/hamburger.svg";
 import minimize from "../assets/minimize.svg";
 import bell from "../assets/bell.svg";
-import notification from "../assets/99+.svg";
 import profileimage from "../assets/short image.svg";
 import down from "../assets/chevron-down.svg";
 
@@ -17,10 +16,12 @@ const Dashboard = () => {
             <img src={logo} alt="Logo Image" className="w-44 mx-3" />
             <img src={hamburger} alt="hamburger icon" className="w-6" />
           </div>
-          <div className="flex justify-center gap-6">
+          <div className="flex justify-center gap-5">
             <img src={minimize} alt="minimize icon" className="w-5" />
-            <img src={bell} alt="bell" className="w-6" />
-            <img src={notification} alt="Notification 99+" className="w-2" />
+            <img src={bell} alt="bell" className="w-4" />
+            <div className="bg-[#FF6F61] rounded-lg w-8 h-4 text-white text-xs">
+              99+
+            </div>
             <img src={profileimage} alt="profile icon" className="w-4" />
             <img src={down} alt="Down Arrow" className="w-4" />
           </div>
