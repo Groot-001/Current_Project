aedcadcadce4519b38bb00c0816296f942b89367
Pratik <pratiikpatel.001@gmail.com>
2026-01-11 21:34:10 +0530

    modify sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,18 @@
+import grid from "../assets/grid.svg";
+
 const Sidebar = () => {
   return (
     <>
       {/* Main Container */}
       <div>
-        <div></div>
+        {/* Containers */}
+        <div className="flex gap-2">
+          <div className="bg-[#A03879] w-1 rounded-2xl"></div>
+          <div className="bg-[#A03879] flex justify-center">
+            <img src={grid} alt="grid icon" className="w-4" />
+            <span>Dashboard</span>
+          </div>
+        </div>
       </div>
     </>
   );
