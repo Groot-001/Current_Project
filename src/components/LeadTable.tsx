a5acf15537500055dbefd0e965f738f3b0bfe0a7
Pratik <pratiikpatel.001@gmail.com>
2026-01-24 21:02:11 +0530

    LeadTable update

--- src/components/LeadTable.tsx
+++ src/components/LeadTable.tsx
@@ -1,8 +1,8 @@
 const LeadTable = () => {
   return (
     <>
-      <div className="w-full bg-[#FFFFFF] rounded-2xl flex flex-col gap-6 shadow-[0_0_16px_#0000000A] p-6">
-        <div className="w-full flex flex-col gap-3 ">
+      <div className="w-3/6 mx-auto bg-[#FFFFFF] rounded-2xl shadow-[0_0_16px_#0000000A] p-6">
+        <div className="flex flex-col gap-6">
           {/* Ttile container */}
           <div>
             <span className="text-[#000000] font-medium text-sm leading-[22px]">
@@ -10,79 +10,79 @@ const LeadTable = () => {
             </span>
           </div>
           {/* Table data container */}
-          <div className="flex flex-col gap-1">
+          <div className="bg-[#FFFFFF] border-[0.4px] border-b-0 border-[#F6EBF2] rounded-t-lg">
             <table className="w-full">
-              <thead className="bg-[#FFFFFF] border-t-[0.4px] rounded-t-lg border-[#F6EBF2] ">
-                <tr className="font-normal text-[#2E2E2E] text-sm leading-[22px]">
-                  <th>S.N</th>
-                  <th>Lead Name</th>
-                  <th>Phone No</th>
-                  <th>Source</th>
-                  <th>Service</th>
-                  <th>Address</th>
-                  <th>Created At</th>
+              <thead>
+                <tr>
+                  <th className="py-2 px-5">S.N</th>
+                  <th className="py-2 px-5">Lead Name</th>
+                  <th className="py-2 px-5">Phone No</th>
+                  <th className="py-2 px-5">Source</th>
+                  <th className="py-2 px-5">Service</th>
+                  <th className="py-2 px-5">Address</th>
+                  <th className="py-2 px-5">Created At</th>
                 </tr>
               </thead>
 
               <tbody>
-                <tr>
-                  <td>01</td>
-                  <td>Sanjay Thapa</td>
-                  <td>98256655235</td>
-                  <td>Facebook</td>
-                  <td>IVF</td>
-                  <td>Kathmandu, Bhaktapur</td>
-                  <td>16/02/2057</td>
+                <tr className="bg-[#F6EBF22E] border-[#F6EBF2] border-[0.4px] border-l-0 border-r-0 border-b-0">
+                  <td className="px-5 py-2">01</td>
+                  <td className="px-5 py-2">Sanjay Thapa</td>
+                  <td className="px-5 py-2">98256655235</td>
+                  <td className="px-5 py-2">Facebook</td>
+                  <td className="px-5 py-2">IVF</td>
+                  <td className="px-5 py-2">Kathmandu, Bhaktapur</td>
+                  <td className="px-5 py-2">16/02/2057</td>
                 </tr>
 
                 <tr>
-                  <td>02</td>
-                  <td>Kavita Sharma</td>
-                  <td>9856235654</td>
-                  <td>Twitter</td>
-                  <td>IVF</td>
-                  <td>Pokhara, Lalitpur</td>
-                  <td>16/02/2057</td>
+                  <td className="px-5 py-2">02</td>
+                  <td className="px-5 py-2">Kavita Sharma</td>
+                  <td className="px-5 py-2">9856235654</td>
+                  <td className="px-5 py-2">Twitter</td>
+                  <td className="px-5 py-2">IVF</td>
+                  <td className="px-5 py-2">Pokhara, Lalitpur</td>
+                  <td className="px-5 py-2">16/02/2057</td>
+                </tr>
+
+                <tr className="bg-[#F6EBF22E]">
+                  <td className="px-5 py-2">03</td>
+                  <td className="px-5 py-2">Dr. Anil Joshi</td>
+                  <td className="px-5 py-2">9756655235</td>
+                  <td className="px-5 py-2">Instagram</td>
+                  <td className="px-5 py-2">IUI</td>
+                  <td className="px-5 py-2">Biratnagar, Janakpur</td>
+                  <td className="px-5 py-2">16/02/2057</td>
+                </tr>
+
+                <tr className="bg-[#F6EBF2]">
+                  <td className="px-5 py-2">04</td>
+                  <td className="px-5 py-2">Nisha Rai</td>
+                  <td className="px-5 py-2">9825566364</td>
+                  <td className="px-5 py-2">WhatsApp</td>
+                  <td className="px-5 py-2">IVF</td>
+                  <td className="px-5 py-2">Nepalgunj, Butwal</td>
+                  <td className="px-5 py-2">16/02/2057</td>
+                </tr>
+
+                <tr className="bg-[#F6EBF22E]">
+                  <td className="px-5 py-2">05</td>
+                  <td className="px-5 py-2">Aarav Gurung</td>
+                  <td className="px-5 py-2">9756542315</td>
+                  <td className="px-5 py-2">Youtube</td>
+                  <td className="px-5 py-2">IVF</td>
+                  <td className="px-5 py-2">Dharan, Itahari</td>
+                  <td className="px-5 py-2">16/02/2057</td>
                 </tr>
 
                 <tr>
-                  <td>03</td>
-                  <td>Dr. Anil Joshi</td>
-                  <td>9756655235</td>
-                  <td>Instagram</td>
-                  <td>IUI</td>
-                  <td>Biratnagar, Janakpur</td>
-                  <td>16/02/2057</td>
-                </tr>
-
-                <tr>
-                  <td>04</td>
-                  <td>Nisha Rai</td>
-                  <td>9825566364</td>
-                  <td>WhatsApp</td>
-                  <td>IVF</td>
-                  <td>Nepalgunj, Butwal</td>
-                  <td>16/02/2057</td>
-                </tr>
-
-                <tr>
-                  <td>05</td>
-                  <td>Aarav Gurung</td>
-                  <td>9756542315</td>
-                  <td>Youtube</td>
-                  <td>IVF</td>
-                  <td>Dharan, Itahari</td>
-                  <td>16/02/2057</td>
-                </tr>
-
-                <tr>
-                  <td>06</td>
-                  <td>Priya Koirala</td>
-                  <td>9756235468</td>
-                  <td>9756235468</td>
-                  <td>IVF</td>
-                  <td>Hetauda, Bhadrapur</td>
-                  <td>16/02/2057</td>
+                  <td className="px-5 py-2">06</td>
+                  <td className="px-5 py-2">Priya Koirala</td>
+                  <td className="px-5 py-2">9756235468</td>
+                  <td className="px-5 py-2">9756235468</td>
+                  <td className="px-5 py-2">IVF</td>
+                  <td className="px-5 py-2">Hetauda, Bhadrapur</td>
+                  <td className="px-5 py-2">16/02/2057</td>
                 </tr>
               </tbody>
             </table>
@@ -95,4 +95,3 @@ const LeadTable = () => {
 
 export default LeadTable;
 
-// box-shadow: 0px 0px 16px 0px #0000000A;
