5b5eadf85c789a548bfa061b982f139de226a3e2
Pratik <pratiikpatel.001@gmail.com>
2026-01-30 16:23:16 +0530

    Update TasksOverview

--- src/components/TasksOverview.tsx
+++ src/components/TasksOverview.tsx
@@ -6,11 +6,16 @@ const TasksOverview = () => {
       {/* Main Container */}
       <div>
         {/* Sub containers */}
-        <div className="flex">
-          <div>
-            <img src={alert_circle} alt="alert-circle icon" />
+        <div className="px-6 pt-6 pb-4">
+          <div className="flex">
+            <img
+              src={alert_circle}
+              alt="alert-circle icon"
+              className="w-5 h-5"
+            />
+            <span>Overdue Tasks</span>
           </div>
-          <div></div>
+          <span>Tasks that are past their due date</span>
         </div>
         {/* sub */}
         <div>2</div>
