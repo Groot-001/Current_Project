64b12139dc143f121e5cef77690a340f5603e18d
Pratik <pratiikpatel.001@gmail.com>
2026-02-04 00:31:41 +0530

    Button added

--- src/components/TasksOverview.tsx
+++ src/components/TasksOverview.tsx
@@ -4,12 +4,12 @@ const TasksOverview = () => {
   return (
     <>
       {/* Main Container */}
-      <div className="flex flex-col rounded-t-[10px] gap-6">
+      <div className="max-w-xs rounded-t-[10px]">
         {/* Overdue Tasks */}
         <div className="flex flex-col rounded-[10px] border-[0.8px] bg-[#FFFFFF] shadow-[0_0_16px_-1px_rgba(22, 22, 22, 0.04)]">
           {/* The rgba value is the arbitary value in the shadow */}
 
-          {/* First Sub Container */}
+          {/* First Sub Container of first container*/}
           <div className="px-6 pt-6 pb-4 flex flex-col gap-1">
             <div className="flex gap-2 items-center">
               <img
@@ -29,13 +29,42 @@ const TasksOverview = () => {
             </div>
           </div>
 
-          {/* Second sub container */}
+          {/* Second sub container of 1st container*/}
           <div className="flex flex-col gap-3 p-4">
-            {/* first box of second container */}
-            <div className="bg-[#F6EBF23D] border-[#FFF1EF] border-[0.8px] rounded-[10px] p-4 flex flex-col gap-3">
-              <div>first</div>
-              <div>second</div>
+            {/* first box of 2nd cont of 1st cont*/}
+            <div className="flex flex-col bg-[#F6EBF23D] border-[#FFF1EF] border-[0.8px] rounded-[10px] p-4 gap-3">
+              {/* inner cont for text / Main div / upper one */}
+              <div className="flex justify-between pr-0 items-start">
+                {/* upper one for wrapping those 2 text box vertically / left one */}
+                <div className="flex flex-col gap-1">
+                  <div>
+                    <span className="text-[#3B3B3B] font-light text-sm tracking-normal">
+                      Follow-up on initial consultation
+                    </span>
+                  </div>
+
+                  {/* lower line text / 2nd line */}
+                  <div>
+                    <span className="text-[#FF6F61] font-normal text-sm tracking-normal">
+                      Sunita Rana
+                    </span>
+                  </div>
+                </div>
+
+                {/* text for in top corner / Right one */}
+                <div className="px-2 pt-[2px] pb-1 bg-[#FFD2CE] rounded-full">
+                  <span className="text-[#B00020] font-light text-[10px] leading-[16px] tracking-normal whitespace-nowrap">
+                    2 days overdue
+                  </span>
+                </div>
+              </div>
+
+              {/* lower container for 2 buttons*/}
+              <div>
+                <span>second lower for 2 button</span>
+              </div>
             </div>
+
             <div>two</div>
             <div>third</div>
           </div>
