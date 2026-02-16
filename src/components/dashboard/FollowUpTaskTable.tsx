5a09550dc8b62ccfd69dbdec361ea397e370a9cb
Pratik <pratiikpatel.001@gmail.com>
2026-02-16 21:04:51 +0530

    FollowupTaskTable updated

--- src/components/dashboard/FollowUpTaskTable.tsx
+++ src/components/dashboard/FollowUpTaskTable.tsx
@@ -1,8 +1,138 @@
+const leads = [
+  {
+    id: 1,
+    name: "Sanjay Thapa",
+    phone: "98256555235",
+    source: "Facebook",
+    service: "IVF",
+    assignedBy: "Suman",
+    followUpDate: "2026-02-15",
+  },
+  {
+    id: 2,
+    name: "Kavita Sharma",
+    phone: "9856235654",
+    source: "Twitter",
+    service: "IVF",
+    assignedBy: "Ravi",
+    followUpDate: "2026-02-15",
+  },
+  {
+    id: 3,
+    name: "Dr. Anil Joshi",
+    phone: "9756655235",
+    source: "Instagram",
+    service: "IUI",
+    assignedBy: "Aarav",
+    followUpDate: "2026-02-15",
+  },
+  {
+    id: 4,
+    name: "Nisha Rai",
+    phone: "9856235654",
+    source: "WhatsApp",
+    service: "IVF",
+    assignedBy: "Karan",
+    followUpDate: "2026-02-15",
+  },
+  {
+    id: 5,
+    name: "Aarav Gurung",
+    phone: "9756542315",
+    source: "Youtube",
+    service: "IVF",
+    assignedBy: "Rajesh",
+    followUpDate: "2026-02-15",
+  },
+  {
+    id: 6,
+    name: "Priya Koirala",
+    phone: "9756235468",
+    source: "Youtube",
+    service: "IVF",
+    assignedBy: "Deepak",
+    followUpDate: "2026-02-15",
+  },
+];
+
+// This is used to define the shape of an object.
+type Column = {
+  header: string;
+  key: keyof (typeof leads)[number] | "index";
+};
+
+// This is for the header which is blueprint for the table
+const FollowUpTaskColumn: Column[] = [
+  {
+    header: "S.N", // what column should be called.
+    key: "index", // what data will it connect to.
+  },
+  {
+    header: "Lead Name",
+    key: "name",
+  },
+  {
+    header: "PhoneNo",
+    key: "phone",
+  },
+  {
+    header: "Up Coming",
+    key: "followUpDate",
+  },
+
+  {
+    header: "Source",
+    key: "source",
+  },
+  {
+    header: "Service",
+    key: "service",
+  },
+  {
+    header: "Assigned By",
+    key: "assignedBy",
+  },
+];
+
 const FollowUpTaskTable = () => {
   return (
     <>
       <div className="rounded-[21px] p-6 bg-[#FFFFFF] shadow-[0px_0px_16px_0px_#0000000A]">
-        
+        <div className="flex flex-col gap-3">
+          <span className="font-normal text-[14px] leading-[22px] text-[#000000]">
+            Follow-up Tasks Today
+          </span>
+          <div className="">
+            <table className="w-full">
+              <thead>
+                {/* So basically the table is one structure and thead is for the table header part, the tr is table row which means we are creating a single row for the header. Inside that row we are craeting th which means table header which is header cell like the placeholder for the data of that row */}
+                <tr>
+                  {FollowUpTaskColumn.map((col) => (
+                    <th key={col.key}>{col.header}</th>
+                  ))}
+                </tr>
+              </thead>
+
+              {/* In the header we have used tr first because we only need one row and all the things will be inside that row only but in the body we need more rows which will be defined by the number of objects in the array so we are mapping the leads array to define the number of rows being used. */}
+              <tbody className="">
+                {leads.map((lead, ind) => (
+                  // Here we are mapping the leads to define the number of rows used in the table.
+
+                  <tr key={lead.id} className="">
+                    {FollowUpTaskColumn.map((col) => (
+                      // Here we are using the followuptaskcolumn because it will decide the number of cols used in the table and for each table we are creating a description cell like placeholder.
+                      <td key={col.key}>
+                        {/* here we are using the col.key === index so that because the key is index which will not match any property from lead so if the value is index then we will just show the index number which will be S.N */}
+                        {col.key === "index" ? ind + 1 : (lead as any)[col.key]}
+                        {/* lead[col.key],this is called dynamic accessing of the data. */}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </>
   );
